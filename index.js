3d48ef1de4b813905b02a45d7808f878c421aefc
Parwiz Ahmadi <parwiz@amazing.nl>
2018-04-12 14:36:14 +0200

    wrong portnumber was used

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3001, function(){
+http.listen(3000, function(){
     console.log('listening on *:3000');
 });
 
